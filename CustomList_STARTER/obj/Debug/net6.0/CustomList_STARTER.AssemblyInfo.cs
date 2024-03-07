160a7d307c9f77aaa0b514aede8a9099c8799475
Yuhan214 <yx6927@rit.edu>
2024-03-07 16:54:54 -0500

    /

--- CustomList_STARTER/obj/Debug/net6.0/CustomList_STARTER.AssemblyInfo.cs
+++ CustomList_STARTER/obj/Debug/net6.0/CustomList_STARTER.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CustomList_STARTER")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+988c972a248243b4efcb438323dced8e8704d788")]
 [assembly: System.Reflection.AssemblyProductAttribute("CustomList_STARTER")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CustomList_STARTER")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
